122512135dc2323e6c943527df251cd292a00eca
MCamus10 <109302595+MCamus10@users.noreply.github.com>
2025-11-04 11:51:18 -0300

    Project section added. Pending adding projects.

---
+++ proyectos.js
@@ -0,0 +1,18 @@
+const proyectosData = [
+    {
+        id: 1,
+        categoriaID: "independiente",
+        titulo: "Diseño Gimnasio",
+        subtitulo: "Estructura de acero",
+        imagen: "./images/foto.JPG",
+        descripcion: "Cálculo estructural..."
+    },
+    {
+        id: 2,
+        categoriaID: "empresa",
+        titulo: "Diseño Gimnasio",
+        subtitulo: "Estructura de acero",
+        imagen: "./images/foto.JPG",
+        descripcion: "Cálculo estructural..."  
+    }
+]
---
+++ render-proyectos.js
@@ -0,0 +1,37 @@
+const contenedoresMap = {
+    "independiente": document.getElementById("contenedor-independiente"),
+    "municipalidad": document.getElementById("contenedor-municipalidad"),
+    "cohesiona": document.getElementById("contenedor-cohesiona"),
+    "civilab": document.getElementById("contenedor-civilab"),
+    "puentes": document.getElementById("contenedor-puentes")
+}
+proyectosData.forEach(proyecto =>{
+    const contenedorDestino = contenedoresMap[proyecto.categoriaID];
+    if (!contenedorDestino){
+        console.warn(`No se encontró contenedor para la categoría: ${proyecto.categoriaID}`);
+    }
+
+    const tarjetaProyecto = document.createElement("div");
+    tarjetaProyecto.classList.add("tarjeta-proyecto");
+    const imagen = document.createElement("img");
+    imagen.src = proyecto.imagen;
+    imagen.alt = proyecto.titulo;
+    imagen.classList.add("tarjeta-imagen");
+    
+    const contenido = document.createElement("div");
+    contenido.classList.add("tarjeta-contenido");
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = proyecto.titulo;
+
+    const descripcion = document.createElement("p");
+    descripcion.textContent = proyecto.descripcion;
+    
+    contenido.appendChild(titulo)
+    contenido.appendChild(descripcion);
+    tarjetaProyecto.appendChild(imagen)
+    tarjetaProyecto.appendChild(contenido);
+    contenedorDestino.appendChild(tarjetaProyecto);
+});
+    
+    
